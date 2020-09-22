ec16038d7d0630f79b312da19f8b324065a16576
e-fialkovskiy <e.fialkovskiy@yandex.ru>
2020-09-22 01:59:10 -0500

    task 4.8 Сделано

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -20,3 +20,36 @@
 """
 
 ip = "192.168.3.1"
+
+
+ip_split = ip.split('.')                    # Разбиваем на октеты
+hex_ip_split_oct1 = int (ip_split[0],16)
+bin_ip_split_oct1 = bin (hex_ip_split_oct1)
+oct1 = bin_ip_split_oct1.lstrip('0b')
+
+hex_ip_split_oct2 = int (ip_split[1],16)
+bin_ip_split_oct2 = bin (hex_ip_split_oct2)
+oct2 = bin_ip_split_oct2.lstrip('0b')
+
+hex_ip_split_oct3 = int (ip_split[2],16)
+bin_ip_split_oct3 = bin (hex_ip_split_oct3)
+oct3 = bin_ip_split_oct3.lstrip('0b')
+
+hex_ip_split_oct4 = int (ip_split[3],16)
+bin_ip_split_oct4 = bin (hex_ip_split_oct4)
+oct4 = bin_ip_split_oct4.lstrip('0b')
+
+
+int_ip1 = int(ip_split[0])
+int_ip2 = int(ip_split[1])
+int_ip3 = int(ip_split[2])
+int_ip4 = int(ip_split[3])
+
+
+print (f'''
+{int_ip1:<9} {int_ip2:<9} {int_ip3:<9} {int_ip4:<9}
+{oct1:>09} {oct2:>09} {oct3:>09} {oct4:>09}''')
+
+
+
+
